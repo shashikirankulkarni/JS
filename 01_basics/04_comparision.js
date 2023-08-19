5e3e093b41ce52cd90ef2c9971574e3a11e5fcc4
Shashikiran <shashikirankulkarni@gmail.com>
2023-08-19 11:50:35 +0000

    Added Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+//console.log(2 > 1);
+//console.log(2 != 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//console.log(null > 0); //During Comparision Operator null gets converted to number i.e 0
+//console.log(null == 0);
+//console.log(null >= 0); //true
+//console.log(null <= 0); //true
+
+//console.log(undefined > 0); //all false
+//console.log(undefined == 0);
+//console.log(undefined >= 0);
+//console.log(undefined <= 0);
+
+// ===
+console.log("2" === 2);
+
