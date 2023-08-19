b529c765e5a2df5d1f6a74054bffa7c983f05c60
Shashikiran <shashikirankulkarni@gmail.com>
2023-08-19 06:39:42 +0000

    Added Variables and Data Types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 56489987;
+let accountEmail = "shashikiran@gmail.com";
+var accountPassword = "test123";
+accountCity = "Jamkhandi";
+
+let accountState;
+
+//accountId = 2;
+accountEmail = "kulkarni@gmail.com";
+accountPassword = "12345";
+accountCity = "Bijapur";
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hello"); //we are using node js not browser
+
+let age = 25;
+let name = "Shashikrian";
+let isLoggedIn = false; 
+
+//number, bigint, string, boolean, null => standalone value(Empty), undefined, symbol=> unique
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
